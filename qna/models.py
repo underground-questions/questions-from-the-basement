0d911f723fc011ae7b9ee53d0a5e2a41c882b5ab
Stacy Zhao <stacyzhao@hotmail.com>
2016-05-17 17:04:58 -0400

    added models

--- qna/models.py
+++ qna/models.py
@@ -1,3 +1,23 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Answer(models.Model):
+    text = models.TextField()
+    owner = models.ForeignKey(User, on_delete=models.CASCADE)
+    votes = models.IntegerField()
+    question = models.ForeignKey('Question', on_delete=models.CASCADE)
+    created = models.DateTimeField(auto_now_add=True)
+
+
+class Question(models.Model):
+    title = models.CharField(max_length=250)
+    description = models.TextField()
+    owner = models.ForeignKey(User, on_delete=models.CASCADE)
+    categories = models.CharField(max_length=250)
+    created = models.DateTimeField(auto_now_add=True)
+
+
+class Owner(models.Model):
+    owner = models.OneToOneField(User, on_delete=models.CASCADE)
+    score = models.IntegerField()
--- underground_questions/settings.py
+++ underground_questions/settings.py
@@ -28,6 +28,7 @@ DEBUG = True
 # Application definition
 
 INSTALLED_APPS = (
+    'qna.apps.QnaConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
